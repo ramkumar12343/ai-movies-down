f1df276f3b100cff540e2fcc259873a2aea3a4ed
ramkumar12343 <bussinesswork2022@gmail.com>
2025-05-13 15:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from sentence_transformers import SentenceTransformer
